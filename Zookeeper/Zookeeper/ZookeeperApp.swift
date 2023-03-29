6154577acce83b01c5077fd9515de8339d2fa583
Dophey <sophietorkelsen@gmail.com>
2023-03-29 14:35:04 +0200

    Add multiple Exercise projects

---
+++ Zookeeper/Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by Sophie on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
